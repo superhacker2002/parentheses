4bc547090ddb60cac0bbbd9fb2bb07976ac4937c
rubiezzy <rubiezzy@student.21-school.ru>
2023-04-04 14:30:46 +0400

    Add tests for IsBalanced func

---
+++ parentheses_test.go
@@ -0,0 +1,57 @@
+package parentheses
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestIsBalanced(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			name:     "Empty string",
+			input:    "",
+			expected: true,
+		},
+		{
+			name:     "Balanced string",
+			input:    "{[{}]()}",
+			expected: true,
+		},
+		{
+			name:     "Unbalanced string",
+			input:    "[(])",
+			expected: false,
+		},
+		{
+			name:     "String with only opening brackets",
+			input:    "{{[[((",
+			expected: false,
+		},
+		{
+			name:     "String with only closing brackets",
+			input:    "))]}",
+			expected: false,
+		},
+		{
+			name:     "Balanced string with other characters",
+			input:    "a(b[c{d}e]f)g",
+			expected: true,
+		},
+		{
+			name:     "Unbalanced string with other characters",
+			input:    "a(b[c{d}e]f)g))){{]]",
+			expected: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			isBalanced := IsBalanced(testCase.input)
+			assert.Equal(t, isBalanced, testCase.expected, "IsBalanced(%q) ", testCase.input)
+		})
+	}
+}
