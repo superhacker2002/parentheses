ad0a260adf451251768cbda55d487d7e494dc4d3
rubiezzy <rubiezzy@student.21-school.ru>
2023-04-05 15:36:27 +0400

    Move rand seed to main

--- cmd/generator/generator.go
+++ cmd/generator/generator.go
@@ -3,10 +3,13 @@ package main
 import (
 	"github.com/superhacker2002/parentheses/internal/controller"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
 	controller.SetRoutes()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	rand.Seed(time.Now().UnixNano())
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
