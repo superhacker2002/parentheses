af728a93b6059576715c4ab8511a3568038ea778
rubiezzy <rubiezzy@student.21-school.ru>
2023-04-05 15:38:04 +0400

    Add tests for GenerateRandomSequence

--- internal/parentheses/parentheses_test.go
+++ internal/parentheses/parentheses_test.go
@@ -2,6 +2,7 @@ package parentheses
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
 )
 
@@ -56,7 +57,34 @@ func TestIsBalanced(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
 			isBalanced := IsBalanced(testCase.input)
-			assert.Equal(t, isBalanced, testCase.expected, "IsBalanced(%q) ", testCase.input)
+			assert.Equal(t, testCase.expected, isBalanced, "IsBalanced(%q) ", testCase.input)
+		})
+	}
+}
+
+func TestGenerateRandomSequence(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    uint
+		expected string
+	}{
+		{
+			name:     "Correct length",
+			input:    10,
+			expected: ")])){({[}(",
+		},
+		{
+			name:     "Zero length",
+			input:    0,
+			expected: "",
+		},
+	}
+	rand.Seed(1)
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			assert.Equal(t, testCase.expected, GenerateRandomSequence(testCase.input),
+				"GenerateRandomSequence(%d) ", testCase.input)
 		})
 	}
 }
