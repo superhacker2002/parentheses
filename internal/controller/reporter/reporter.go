f06fff07c5844f590de058486549b0f8ad3e9f40
rubiezzy <rubiezzy@student.21-school.ru>
2023-04-06 17:34:32 +0400

    Add methods for report output

---
+++ internal/controller/reporter/reporter.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/superhacker2002/parentheses/internal/parentheses"
+)
+
+type client interface {
+	Generate(length uint) (string, error)
+}
+
+type Reporter struct {
+	Client client
+}
+
+func (r Reporter) Do() error {
+	lengths := [3]uint{2, 4, 8}
+	for _, length := range lengths {
+		if err := r.report(length); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r Reporter) report(length uint) error {
+	var balancedNum int
+	for i := 0; i < 1000; i++ {
+		resp, err := r.Client.Generate(length)
+		if err != nil {
+			return err
+		}
+		if parentheses.IsBalanced(resp) {
+			balancedNum++
+		}
+	}
+	percentBalanced := float64(balancedNum) / 10
+	fmt.Printf("For length %d, %f percent of generated strings are balanced\n", length, percentBalanced)
+	return nil
+}
