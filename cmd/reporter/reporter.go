716bf44a515662675247ce4553c84f3181da6aec
rubiezzy <rubiezzy@student.21-school.ru>
2023-04-11 14:49:14 +0400

    Add passing server address through environment variables

--- cmd/reporter/reporter.go
+++ cmd/reporter/reporter.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/superhacker2002/parentheses/internal/client"
 	"github.com/superhacker2002/parentheses/internal/controller/reporter"
 	"github.com/superhacker2002/parentheses/internal/parentheses"
 	"log"
+	"os"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("no .env file found")
+	}
+}
+
 func main() {
-	client := client.New("http://localhost:8080")
+	serverAddr, exists := os.LookupEnv("SERVER_ADDR")
+	if !exists {
+		log.Fatal("no server address provided in .env file")
+	}
+	client := client.New("http://" + serverAddr)
 	reporter := reporter.New(parentheses.New(), client)
 	if err := reporter.Do(); err != nil {
 		log.Fatal(err)
